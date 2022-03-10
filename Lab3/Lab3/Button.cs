8198ae9022471f806754918c727466a723b816ef
splashgy <potatoelesha@gmail.com>
2022-03-10 11:13:23 +0300

    Init Lab3 commit

---
+++ Lab3/Lab3/Button.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab3
+{
+    internal abstract class Button
+    {
+        public abstract void Execute();
+        public abstract void Cancel();
+        public abstract void Name();
+    }
+}
